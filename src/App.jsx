bb7e69bf8edf82fef828deee7fd69a41cb439de7
asheshjyotii <asheshjyotimajumdar3@gmail.com>
2026-09-21 15:18:03 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import input from 'daisyui/components/input'
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [Input, setInput] = useState("")
+  let tasks = [];
+
+  return (
+    
+    <div>
+      <h1>hi</h1>
+      <form action="">
+        
+        <input type="text" name="enter new task" id="task_input"  />
+        
+        <button onClick={(e)=>{
+          e.preventDefault()
+          const data = {task : document.querySelector('#task_input').value}
+          tasks.push(data);
+          console.log(tasks)
+        }}>add</button>
+        <input type="text" id="delete" />
+        <button onClick={(e)=>{
+          e.preventDefault()
+          let val=document.querySelector('#delete').value
+          let index = tasks.indexOf(val)
+
+          tasks.splice(index,1)
+          console.log(tasks)
+        }} >delete</button>
+
+
+        <p id='tasks'>{tasks.values()}</p>
+      </form>
+    </div>
+  )
+}
+
+export default App
